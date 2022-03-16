9e16e0973b0e2f80abf14c41f6f65528bada1a45
shimnamisan <mikan.sup.all@gmail.com>
2022-03-17 06:20:58 +0900

    [update] 計算処理途中

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { camera, battery } from './datgaModule.js';
 const $ = document;
 const target = $.getElementById("target");
 
@@ -79,166 +80,6 @@ const accessDomLists = {
     batteryArea: $.getElementById("batteryArea"),
 }
 
-const battery =
-    [
-        {
-            "batteryName": "WKL-78", // 電池名
-            "capacityAh": 2.3, // 容量（Ah）
-            "voltage": 14.4,   // 電圧
-            "maxDraw": 3.2,    // 最大放電電流
-            "endVoltage": 10,  // 終止電圧
-        },
-        {
-            "batteryName": "WKL-140",
-            "capacityAh": 4.5,
-            "voltage": 14.4,
-            "maxDraw": 9.2,
-            "endVoltage": 5,
-        },
-        {
-            "batteryName": "Wmacro-78",
-            "capacityAh": 2.5,
-            "voltage": 14.5,
-            "maxDraw": 10,
-            "endVoltage": 5,
-        },
-        {
-            "batteryName": "Wmacro-140",
-            "capacityAh": 3.6,
-            "voltage": 14.4,
-            "maxDraw": 14,
-            "endVoltage": 5,
-        },
-        {
-            "batteryName": "IOP-E78",
-            "capacityAh": 6.6,
-            "voltage": 14.4,
-            "maxDraw": 10.5,
-            "endVoltage": 8,
-        },
-        {
-            "batteryName": "IOP-E140",
-            "capacityAh": 9.9,
-            "voltage": 14.4,
-            "maxDraw": 14,
-            "endVoltage": 10,
-        },
-        {
-            "batteryName": "IOP-E188",
-            "capacityAh": 13.2,
-            "voltage": 14.4,
-            "maxDraw": 14,
-            "endVoltage": 11,
-        },
-        {
-            "batteryName": "RYN-C65",
-            "capacityAh": 4.9,
-            "voltage": 14.8,
-            "maxDraw": 4.9,
-            "endVoltage": 11,
-        },
-        {
-            "batteryName": "RYN-C85",
-            "capacityAh": 6.3,
-            "voltage": 14.4,
-            "maxDraw": 6.3,
-            "endVoltage": 12,
-        },
-        {
-            "batteryName": "RYN-C140",
-            "capacityAh": 9.8,
-            "voltage": 14.8,
-            "maxDraw": 10,
-            "endVoltage": 12,
-        },
-        {
-            "batteryName": "RYN-C290",
-            "capacityAh": 19.8,
-            "voltage": 14.4,
-            "maxDraw": 14,
-            "endVoltage": 12,
-        }]
-    ;
-
-const camera =
-    [
-        {
-            "brand": "Cakon", // メーカー
-            "model": "ABC 3000M", // 製品名
-            "powerConsumptionWh": 35.5, // カメラの消費電力
-        },
-        {
-            "brand": "Cakon",
-            "model": "ABC 5000M",
-            "powerConsumptionWh": 37.2,
-        },
-        {
-            "brand": "Cakon",
-            "model": "ABC 7000M",
-            "powerConsumptionWh": 39.7,
-        },
-        {
-            "brand": "Cakon",
-            "model": "ABC 9000M",
-            "powerConsumptionWh": 10.9,
-        },
-        {
-            "brand": "Cakon",
-            "model": "ABC 9900M",
-            "powerConsumptionWh": 15.7,
-        },
-        {
-            "brand": "Go MN",
-            "model": "UIK 110C",
-            "powerConsumptionWh": 62.3,
-        },
-        {
-            "brand": "Go MN",
-            "model": "UIK 210C",
-            "powerConsumptionWh": 64.3,
-        },
-        {
-            "brand": "Go MN",
-            "model": "UIK 230C",
-            "powerConsumptionWh": 26.3,
-        },
-        {
-            "brand": "Go MN",
-            "model": "UIK 250C",
-            "powerConsumptionWh": 15.3,
-        },
-        {
-            "brand": "Go MN",
-            "model": "UIK 270C",
-            "powerConsumptionWh": 20.3,
-        },
-        {
-            "brand": "VANY",
-            "model": "CEV 1100P",
-            "powerConsumptionWh": 22,
-        },
-        {
-            "brand": "VANY",
-            "model": "CEV 1300P",
-            "powerConsumptionWh": 23,
-        },
-        {
-            "brand": "VANY",
-            "model": "CEV 1500P",
-            "powerConsumptionWh": 24,
-        },
-        {
-            "brand": "VANY",
-            "model": "CEV 1700P",
-            "powerConsumptionWh": 25,
-        },
-        {
-            "brand": "VANY",
-            "model": "CEV 1900P",
-            "powerConsumptionWh": 26,
-        }]
-    ;
-
 class Brand {
     static initialize() {
         const target = accessDomLists.brandArea;
@@ -250,6 +91,11 @@ class Brand {
             <option value="${brandArry[i].brand}">${brandArry[i].brand}</option>
             `
         }
+
+        target.addEventListener('change', (event) => {
+            Calculate.selectBrand(event);
+            Calculate.allCalcurate(event);
+        });
     }
 }
 
@@ -267,6 +113,54 @@ class BatteryModel {
     }
 }
 
+class ChooseBatteryView {
+
+    static createBatteryList() {
+        const target = $.accessDomLists.batteryArea;
+    }
+}
+
+class Calculate {
+
+    // 計算された結果を格納する配列
+    static calculate_result = [];
+
+    static selectBrand(event) {
+        this.calculate_result = [];
+
+        // select要素で選択された値を取得する
+        let brand_name = event.target.value;
+
+        // ブランド名を元にcameraのデータから一致するものを抽出
+        let new_model_lists = camera.filter((item) => item.brand === brand_name);
+
+        this.calculate_result.push(...new_model_lists);
+
+        const target = accessDomLists.modelArea;
+        target.innerHTML = "";
+        for (let i = 0; i < new_model_lists.length; i++) {
+            target.innerHTML += `
+            <option value="${new_model_lists[i].model}">${new_model_lists[i].model}</option>
+            `
+        }
+
+    }
+
+    static selectModel() {
+
+    }
+
+    static selectInputPowerConsumption() {
+
+    }
+
+    static allCalcurate(event) {
+
+        for (let i = 0; i < this.calculate_result.length; i++) {
+            console.log(this.calculate_result[i].powerConsumptionWh)
+        }
+    }
+}
 
 Brand.initialize();
 BatteryModel.initialize("Cakon")
